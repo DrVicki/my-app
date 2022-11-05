e6deea610bed8fadb24ac29bd70d07cf054407c9
Dr Vicki Bealman <drvicki.bealman@gmail.com>
2022-11-05 15:37:23 -0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>Hello LearningSource!</div>;
+};
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLDivElement
+);
+root.render(<App />);
+
